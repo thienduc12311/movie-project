c0a33824d31172ba2819062b5d5df19aa85b7314
Mac <thienduc12311@gmail.com>
2020-10-12 16:25:51 +0700

    fix option sign up

--- src/components/MovieTab/index.js
+++ src/components/MovieTab/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableContainer from '@material-ui/core/TableContainer';
@@ -7,124 +7,130 @@ import TableCell from '@material-ui/core/TableCell';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
 import moment from 'moment';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 
 import './styles.scss';
 
-const MovieTab = ({ movie, isBigScreen }) => {
-    const [isDropdownOpened, setIsDropdownOpened] = useState(false);
+const MovieTab = ({movie, isBigScreen}) => {
+  const [isDropdownOpened, setIsDropdownOpened] = useState(false);
 
-    const handleFilterDateOptions = (dateTimeArray) => {
-        let dateArray = [];
-        dateTimeArray.forEach(item => {
-            let currentDate = moment(item.ngayChieuGioChieu).format('ll');
-            if (dateArray.length === 0)
-                dateArray.push(currentDate);
-            else {
-                let index = dateArray.findIndex(date => date === currentDate)
-                if (index === -1)
-                    dateArray.push(currentDate);
-            }
-        })
-        if (!isBigScreen && dateArray.length > 2)
-            dateArray = dateArray.slice(-2);
-        return dateArray;
-    }
+  const handleFilterDateOptions = (dateTimeArray) => {
+    let dateArray = [];
+    dateTimeArray.forEach((item) => {
+      let currentDate = moment(item.ngayChieuGioChieu).format('ll');
+      if (dateArray.length === 0) dateArray.push(currentDate);
+      else {
+        let index = dateArray.findIndex((date) => date === currentDate);
+        if (index === -1) dateArray.push(currentDate);
+      }
+    });
+    if (!isBigScreen && dateArray.length > 2) dateArray = dateArray.slice(-2);
+    return dateArray;
+  };
 
-    const handleFilterTimeOptions = (dateTimeArray, date) => {
-        const rawArray = dateTimeArray.filter(item => moment(item.ngayChieuGioChieu).format('ll') === date);
-        const timeArray = rawArray.map(item => moment(item.ngayChieuGioChieu).format('LT'));
-        return timeArray;
-    }
+  const handleFilterTimeOptions = (dateTimeArray, date) => {
+    const rawArray = dateTimeArray.filter(
+      (item) => moment(item.ngayChieuGioChieu).format('ll') === date
+    );
+    const timeArray = rawArray.map((item) => moment(item.ngayChieuGioChieu).format('LT'));
+    return timeArray;
+  };
 
-    const renderDateTimeField = (dateTimeArray) => {
-        const dateArray = handleFilterDateOptions(dateTimeArray);
-        return dateArray.map((date, index) => (
-            <Box margin={1} key={index}>
-                <Table size="small" aria-label="purchases">
-                    <TableBody>
-                        <TableRow>
-                            <TableCell style={{ width: 100, padding: 5 }}>
-                                <h5 style={{ margin: 0 }}>{date}</h5>
-                            </TableCell>
-                            <TableCell style={{ width: 50 }}></TableCell>
-                            <TableCell>
-                                <Grid container spacing={2}>
-                                    {renderTimeField(dateTimeArray, date)}
-                                </Grid>
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </Box>
-        ))
-    }
+  const renderDateTimeField = (dateTimeArray) => {
+    const dateArray = handleFilterDateOptions(dateTimeArray);
+    return dateArray.map((date, index) => (
+      <Box margin={1} key={index}>
+        <Table size="small" aria-label="purchases">
+          <TableBody>
+            <TableRow>
+              <TableCell style={{width: 100, padding: 5}}>
+                <h5 style={{margin: 0}}>{date}</h5>
+              </TableCell>
+              <TableCell style={{width: 50}}></TableCell>
+              <TableCell>
+                <Grid container spacing={2}>
+                  {renderTimeField(dateTimeArray, date)}
+                </Grid>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Box>
+    ));
+  };
 
-    let settings;
-    const renderTimeField = (dateTimeArray, date) => {
-        const timeArray = handleFilterTimeOptions(dateTimeArray, date);
-        if (isBigScreen)
-            settings = {
-                xs: 12,
-                sm: 4,
-                md: 3,
-                lg: 2,
-            };
-        else
-            settings = {
-                xs: 4
-            };
-        return timeArray.map((time, index) => (
-            <Grid
-                item
-                key={index}
-                onClick={() => handleClick(date, time, dateTimeArray)}
-                {...settings}
+  let settings;
+  const renderTimeField = (dateTimeArray, date) => {
+    const timeArray = handleFilterTimeOptions(dateTimeArray, date);
+    if (isBigScreen)
+      settings = {
+        xs: 12,
+        sm: 4,
+        md: 3,
+        lg: 2,
+      };
+    else
+      settings = {
+        xs: 4,
+      };
+    return timeArray.map((time, index) => (
+      <Grid
+        item
+        key={index}
+        onClick={() => handleClick(date, time, dateTimeArray)}
+        {...settings}
+      >
+        <span className="movie-time-selector">{time}</span>
+      </Grid>
+    ));
+  };
+
+  const handleClick = (date, time, dateTimeArray) => {
+    const showtimeInfo = dateTimeArray.find(
+      (item) =>
+        moment(item.ngayChieuGioChieu).format('ll') === date &&
+        moment(item.ngayChieuGioChieu).format('LT') === time
+    );
+    console.log(showtimeInfo);
+  };
+
+  return (
+    <TableContainer style={{marginBottom: 10}}>
+      <Table stickyHeader aria-label="sticky table">
+        <TableBody>
+          <TableRow onClick={() => setIsDropdownOpened(!isDropdownOpened)}>
+            <TableCell
+              className={
+                isBigScreen ? 'movie-logo-container-bigscreen' : 'movie-logo-container'
+              }
             >
-                <span className="movie-time-selector">{time}</span>
-            </Grid>
-        ))
-    }
-
-    const handleClick = (date, time, dateTimeArray) => {
-        const showtimeInfo = dateTimeArray.find(item =>
-            moment(item.ngayChieuGioChieu).format('ll') === date && moment(item.ngayChieuGioChieu).format('LT') === time
-        );
-        console.log(showtimeInfo);
-    }
-
-    return (
-        <TableContainer style={{ marginBottom: 10 }} >
-            <Table stickyHeader aria-label="sticky table">
-                <TableBody>
-                    <TableRow onClick={() => setIsDropdownOpened(!isDropdownOpened)}>
-                        <TableCell
-                            className={isBigScreen ? "movie-logo-container-bigscreen" : "movie-logo-container"}
-                        >
-                            <NavLink to={`/movie/id=${movie.maPhim}`}>
-                                <img className="movie-logo" src={movie.hinhAnh} />
-                            </NavLink>
-                        </TableCell>
-                        <TableCell style={{ border: 'none', padding: '16px 0 16px 15px' }}>
-                            <h4 className="movie-name">
-                                <span className="movie-label">C16</span>
-                                {movie.tenPhim}
-                            </h4>
-                            <p className="movie-more-details">120 minutes - TIX 9.3 - IMDb 0</p>
-                        </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell style={{ paddingBottom: 0, paddingTop: 0, border: 'none' }} colSpan={6}>
-                            <Collapse in={isDropdownOpened} timeout="auto" unmountOnExit>
-                                {renderDateTimeField(movie.lstLichChieuTheoPhim)}
-                            </Collapse>
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
-}
+              <NavLink to={`/movie/id=${movie.maPhim}`}>
+                <img className="movie-logo" src={movie.hinhAnh} />
+              </NavLink>
+            </TableCell>
+            <TableCell style={{border: 'none', padding: '16px 0 16px 15px'}}>
+              <h4 className="movie-name">
+                <span className="movie-label">C16</span>
+                {movie.tenPhim}
+              </h4>
+              <p className="movie-more-details">120 minutes - TIX 9.3 - IMDb 0</p>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell
+              style={{paddingBottom: 0, paddingTop: 0, border: 'none'}}
+              colSpan={6}
+            >
+              <Collapse in={isDropdownOpened} timeout="auto" unmountOnExit>
+                {renderDateTimeField(movie.lstLichChieuTheoPhim)}
+              </Collapse>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default MovieTab;
--- src/containers/HomePage/MovieNav/index.js
+++ src/containers/HomePage/MovieNav/index.js
@@ -1,37 +1,35 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Tabs } from 'antd';
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {Tabs} from 'antd';
 import CinemaField from './CinemaField';
-import { getCinemaComplexInfo } from '../../../redux/actions/movieAction';
+import {getCinemaComplexInfo} from '../../../redux/actions/movieAction';
 
 import './styles.scss';
 
-const { TabPane } = Tabs;
+const {TabPane} = Tabs;
 
 const MovieNav = () => {
-    const cinemaComplexInfo = useSelector(state => state.movieReducer.cinemaComplexInfo);
-    const dispatch = useDispatch();
-
-    useEffect(() => dispatch(getCinemaComplexInfo()), [])
-
-    return (
-        <Tabs
-            defaultActiveKey="0"
-            tabPosition="top"
-            style={{ height: '100%', width: '100%' }}
-            centered
-            className="movie-collection"
+  const cinemaComplexInfo = useSelector((state) => state.movieReducer.cinemaComplexInfo);
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(getCinemaComplexInfo()), []);
+  return (
+    <Tabs
+      defaultActiveKey="0"
+      tabPosition="top"
+      style={{height: '100%', width: '100%'}}
+      centered
+      className="movie-collection"
+    >
+      {[...Array.from({length: cinemaComplexInfo?.length}, (v, i) => i)].map((i) => (
+        <TabPane
+          key={i}
+          tab={<img className="cinema-complex__logo" src={cinemaComplexInfo[i].logo} />}
         >
-            {[...Array.from({ length: cinemaComplexInfo?.length }, (v, i) => i)].map(i => (
-                <TabPane
-                    key={i}
-                    tab={<img className="cinema-complex__logo" src={cinemaComplexInfo[i].logo} />}
-                >
-                    <CinemaField cinemaList={cinemaComplexInfo[i].lstCumRap} />
-                </TabPane>
-            ))}
-        </Tabs>
-    )
-}
+          <CinemaField cinemaList={cinemaComplexInfo[i].lstCumRap} />
+        </TabPane>
+      ))}
+    </Tabs>
+  );
+};
 
 export default MovieNav;
